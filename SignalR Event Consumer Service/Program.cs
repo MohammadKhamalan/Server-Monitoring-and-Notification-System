223f48c7e00770d8f5e08f00c1be6c6f46a6a479
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-05-24 02:57:21 +0300

    Task 3 : that connects to a SignalR hub and prints

--- SignalR Event Consumer Service/Program.cs
+++ SignalR Event Consumer Service/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SignalR_Event_Consumer_Service;
+using SignalR_Event_Consumer_Service.Config;
+using SignalR_Event_Consumer_Service.Interfaces;
+
+namespace SignalRAlertConsumer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var configHelper = new ConfigurationHelper();
+        string signalRHubUrl = configHelper.GetSignalRHubUrl();
+
+        ISignalRClient signalRClient = new SignalRClient(signalRHubUrl);
+        signalRClient.Start();
+
+        signalRClient.RegisterAlertHandler(alert =>
+        {
+            Console.WriteLine(alert);
+        });
+
+        Console.ReadLine();
+    }
+}
--- SignalRServer/Startup.cs
+++ SignalRServer/Startup.cs
@@ -27,5 +27,6 @@ namespace SignalRServer
                 endpoints.MapHub<AlertHub>("/alertHub");
             });
         }
+
     }
 }
